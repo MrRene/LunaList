a1dfd6e2663598bd7db4b59491237fb03dee875a
rene <rene.van.der.horst@lunatech.com>
2014-12-09 15:39:49 +0100

    Upgrade 2.3.7 and advice changes

--- app/models/Employee.scala
+++ app/models/Employee.scala
@@ -1,16 +1,16 @@
 package models
 
-case class Employee(id: Int, name: String, email: String, twitterHandle: String)
+case class Employee(id: Int, name: String, email: String, twitterHandle: Option[String])
 
 object Employee {
 
     var employees = Set(
-      Employee(1,"Rene van der Horst","rene.van.der.horst@lunatech.com","None"),
-      Employee(2,"Sander Ufkes","sander.ufkes@lunatech.com","None"),
-      Employee(3,"Thomas Meijer","thomas.meijer@lunatech.com","@TM"),
-      Employee(4,"Erik Bakker","erik.bakker@lunatech.com","@EB"),
-      Employee(5,"Wolfert de Kraker","wolfert.de.kraker@lunatech.com","@WdK"),
-      Employee(6,"Nicolas Leroux","nicolas.leroux@lunatech.com","@LR")
+      Employee(1,"Rene van der Horst","rene.van.der.horst@lunatech.com",None ),
+      Employee(2,"Sander Ufkes","sander.ufkes@lunatech.com",None),
+      Employee(3,"Thomas Meijer","thomas.meijer@lunatech.com",Some("@TM")),
+      Employee(4,"Erik Bakker","erik.bakker@lunatech.com",Some("@EB")),
+      Employee(5,"Wolfert de Kraker","wolfert.de.kraker@lunatech.com",Some("@WdK")),
+      Employee(6,"Nicolas Leroux","nicolas.leroux@lunatech.com",Some("@LR"))
     )
 
   def findAll = employees.toList.sortBy(_.id)
--- build.sbt
+++ build.sbt
@@ -1,12 +1,13 @@
-name := "LunaList"
+import play.PlayScala
+
+name := """LunaList"""
 
 version := "1.0-SNAPSHOT"
 
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.11.1"
+
 libraryDependencies ++= Seq(
-  jdbc,
-  anorm,
-  cache,
   "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
 )
-
-play.Project.playScalaSettings
