5d65889311a2331b7e91a91e49f1cde120664c32
rene <rene.van.der.horst@lunatech.com>
2014-12-09 17:43:34 +0100

    Employeeform

--- app/controllers/Employees.scala
+++ app/controllers/Employees.scala
@@ -1,9 +1,21 @@
 package controllers
 
-import models.Employee
 import play.api.mvc._
+import play.api.mvc.Flash
+import play.api.data._
+import play.api.data.Forms._
+import models.Employee
 
 object Employees extends Controller {
+
+  private val employeeForm: Form[Employee] = Form(
+    mapping(
+      "id" -> number.verifying("validation.id.duplicate", Employee.findById(_).isEmpty),
+      "name" -> text,
+      "email" -> text,
+      "twitterHandle" -> optional(text)
+    ) (Employee.apply)(Employee.unapply)
+  )
 
   def showEmployees = Action { implicit request =>
 
@@ -19,7 +31,18 @@ object Employees extends Controller {
     }.getOrElse(NotFound)
   }
 
-  def editEmployee = Action {
+  def editEmployee = Action { implicit request =>
+    val newEmployeeForm = employeeForm.bindFromRequest()
+
+    /*
+    newEmployeeForm.fold(
+
+      hasErrors = { form =>
+        Redirect(routes.Employees.editEmployee)
+      }
+
+    )
+    */
     Ok("editEmployee")
   }
 }
